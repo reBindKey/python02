f023805bdfa26db462784a263fac8e20c1e73447
reBindKey <108424431+reBindKey@users.noreply.github.com>
2023-01-11 01:06:27 -0500

    Create rounding.py

---
+++ rounding.py
@@ -0,0 +1,8 @@
+# this program will help me make sense of how rounding works with integers in python
+
+x = int('1234567' [6]) # int = 7, array index 6
+y = -round(2.1) # int = -2
+z = x//y # int = 7/-2 => z = -3.5 => -4 (floor = towards -infinity)
+print(z)
+
+# the 
